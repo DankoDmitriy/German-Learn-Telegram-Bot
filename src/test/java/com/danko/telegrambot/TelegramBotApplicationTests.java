4b1282236132baaf72184e26b28fef9ea2ad0ad8
Dmitriy Danko <danko.dima@gmail.com>
2022-08-19 16:57:45 +0300

    Init Spring application

---
+++ src/test/java/com/danko/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danko.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
